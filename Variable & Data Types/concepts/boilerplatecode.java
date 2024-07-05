6421d5bcf6dc73f3320d82ec9362776d8fec06aa
Tanmay Bharambe <bharambetr2002@gmail.com>
2024-07-06 00:05:58 +0530

    Reset

--- Variable & Data Types/concepts/boilerplatecode.java
+++
@@ -1,8 +0,0 @@
-// Description: Boilerplate code for java
-// Boilerplate code is a code that can be used over and over again and is compulsary in java code 
-
-public class boilerplatecode {
-    public static void main(String[] args) {
-        // main functon is always required for java code to compile
-    }
-}
