0d95336fcadfebee198ffaf9da3ba3a33408122b
Tanmay Bharambe <bharambetr2002@gmail.com>
2024-07-18 20:50:16 +0530

    update-loops

--- Practice Questions/PQ14.java
+++ Practice Questions/PQ14.java
@@ -4,17 +4,15 @@ import java.util.Scanner;
 
 public class PQ14 {
     public static void main(String[] args) {
-
         Scanner scn = new Scanner(System.in);
         System.out.println("Enter the number : ");
-
         do {
             int a = scn.nextInt();
             if (a % 10 == 0) {
                 break;
             }
             System.out.println(a);
-
         } while (true);
+        scn.close();
     }
 }
